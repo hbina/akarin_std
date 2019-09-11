bc8b5554caef2230f8a2a07ddbbd3c0679e6d06e
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-09-11 13:53:56 -0400

    Updates to vector...
    A bunch of new methods and no leaks!

--- include/tests/vector_test.hpp
+++ include/tests/vector_test.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
 #include "doctest/doctest.h"
-
 #include "vector.hpp"
 
-constexpr std::size_t AKARIN_ITERATION = 10000;
+constexpr std::size_t AKARIN_ITERATION = 300;
 
 TEST_CASE("test akarin::vector push_back")
 {
@@ -13,9 +12,9 @@ TEST_CASE("test akarin::vector push_back")
     {
         vec.push_back(ctr);
     }
-    for (std::size_t ctr = 0; ctr < AKARIN_ITERATION; ctr++)
+    for (std::size_t ctr2 = 0; ctr2 < AKARIN_ITERATION; ctr2++)
     {
-        CHECK(vec[ctr] == ctr);
+        CHECK(vec[ctr2] == ctr2);
     }
 };
 
@@ -30,4 +29,14 @@ TEST_CASE("test akarin::vector custom growth rate")
     {
         CHECK(vec[ctr] == ctr);
     }
+};
+
+TEST_CASE("test akarin::vector resize correctness")
+{
+    akarin::vector<std::size_t> vec;
+    for (std::size_t ctr = 0; ctr < AKARIN_ITERATION; ctr++)
+    {
+        vec.push_back(ctr);
+    }
+    CHECK(vec.size() == AKARIN_ITERATION);
 };
--- include/vector.hpp
+++ include/vector.hpp
@@ -8,74 +8,96 @@ struct quotient
     static constexpr FP_type value = (FP_type)p / q;
 };
 
-std::size_t calculate_growth(const std::size_t p_cap, const double p_rate)
-{
-    double old_cap = static_cast<double>(p_cap) + 1;
-    std::size_t new_size = static_cast<std::size_t>(old_cap * p_rate);
-    std::cout << "old_cap:" << old_cap << " p_rate:" << p_rate << " new_size:" << new_size << std::endl;
-    return new_size;
-};
-
 namespace akarin
 {
 template <typename T, std::size_t P = 16, std::size_t Q = 10>
 struct vector
 {
+    using value_type = T;
+    using size_type = std::size_t;
     static constexpr double growth_rate = static_cast<double>(P) / static_cast<double>(Q);
     constexpr vector() = default;
-    constexpr vector(const vector &&p_vec)
+    ~vector()
     {
         clear();
-        // not enough capacity to hold content, enlarge
-        if (capacity < p_vec.length)
-            resize(calculate_growth(capacity, growth_rate));
-        for (std::size_t ctr = 0; ctr < p_vec.length; ctr++)
-            push_back(p_vec.data[ctr]);
-    };
+    }
 
     T &operator[](const std::size_t p_idx) noexcept
     {
         return data[p_idx];
     };
 
+    T &operator[](const std::size_t p_idx) const noexcept
+    {
+        return data[p_idx];
+    };
+
     void push_back(const T &p_t) noexcept
     {
-        if (length == capacity)
-            resize(calculate_growth(capacity, growth_rate));
-        data[length++] = p_t;
+        if (len == cap)
+            reserve(calculate_growth(cap, growth_rate));
+        data[len++] = p_t;
     };
 
     void push_back(T &&p_t) noexcept
     {
-        if (length == capacity)
-            resize(calculate_growth(capacity, growth_rate));
-        data[length++] = std::move(p_t);
+        if (len == cap)
+            reserve(calculate_growth(cap, growth_rate));
+        data[len++] = std::move(p_t);
     };
 
-    void resize(const std::size_t p_cap) noexcept
+    void reserve(const std::size_t p_cap) noexcept
     {
-        T *new_data = (T *)realloc(data, p_cap * sizeof(T));
+        T *new_data = (T *)malloc(p_cap * sizeof(T));
         if (!new_data)
             std::terminate(); // no memory, your runtime is fked >3<
-        for (std::size_t ctr = 0; ctr < length; ctr++)
+        for (std::size_t ctr = 0; ctr < len; ctr++)
         {
             new_data[ctr] = data[ctr];
         }
+        free(data);
         data = new_data;
-        capacity = p_cap;
+        cap = p_cap;
     };
 
     void clear() noexcept
     {
         free(data);
         data = nullptr;
-        length = 0;
-        capacity = 0;
+        len = 0;
+        cap = 0;
     };
+
+    std::size_t size() const noexcept
+    {
+        return len;
+    };
+
+    std::size_t capacity() const noexcept
+    {
+        return cap;
+    };
+
+    bool empty() const noexcept
+    {
+        return len == 0;
+    };
+
+    void shrink_to_fit() noexcept
+    {
+        reserve(len);
+    }
 
 private:
     T *data = nullptr;
-    std::size_t length = 0;
-    std::size_t capacity = 0;
+    std::size_t len = 0;
+    std::size_t cap = 0;
+
+    std::size_t calculate_growth(const std::size_t p_cap, const double p_rate)
+    {
+        double old_cap = static_cast<double>(p_cap);
+        std::size_t new_size = static_cast<std::size_t>(old_cap * p_rate);
+        return new_size + 1;
+    };
 };
 }; // namespace akarin
