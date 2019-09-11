f760252d4649595f55765a89140d97c9d1e14715
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2019-09-11 19:54:40 -0400

    Added monoid<T>

---
+++ include/monoid.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace akarin
+{
+template <
+    typename T,
+    T (*Op)(const T &, const T &),
+    T Identity>
+class monoid
+{
+    T member_value;
+
+public:
+    using value_type = T;
+    constexpr monoid() : member_value(Identity){};
+    constexpr monoid(const T &&t) : member_value(std::move(t)){};
+    constexpr monoid operator+(const monoid &t)
+    {
+        if (t == Identity)
+            return *this;
+        return monoid(
+            Op(t.member_value, member_value));
+    };
+
+    operator T() const
+    {
+        return member_value;
+    }
+};
+}; // namespace akarin
